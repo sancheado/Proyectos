4dbba07f258bf1f243c6bd667aafa3e11620731b
Jose Sanchez Mateo <el_render@hotmail.com>
2020-03-08 12:56:33 +0100

    moda terminada, falta media y mediana

--- MiWeb/Stadistics-about-text-files.aspx.vb
+++ MiWeb/Stadistics-about-text-files.aspx.vb
@@ -21,23 +21,30 @@ Public Class Stadistics_about_text_files
                         spOculto.InnerHtml = fname & "<i class='fas fa-file-upload miI'></i>"
 
                         'Guardamos el archivo subido para su posterior analisis.
-                        Dim location As String = "C:\Users\Sanchez\source\repos\MyWebSite\MiWeb\" & fname
-                        file.SaveAs(Path.Combine("C:\Users\Sanchez\source\repos\MyWebSite\MiWeb", fname))
+                        Dim location As String = "C:\Users\el_re\source\repos\Proyectos\MiWeb\" & fname
+                        'Dim location As String = "C:\Users\Sanchez\source\repos\MyWebSite\MiWeb\" & fname
+                        file.SaveAs(Path.Combine("C:\Users\el_re\source\repos\Proyectos\MiWeb\", fname))
+                        'file.SaveAs(Path.Combine("C:\Users\Sanchez\source\repos\MyWebSite\MiWeb", fname))
 
-                        Dim MiArray() As String, VDatos As String
+                        Dim MiArray() As String, VDatos As String, ModeArray() As Integer
 
                         Dim sr As StreamReader = New StreamReader(location)
                         Dim full_text As StreamReader = New StreamReader(location)
                         Dim line As String, count_line As Integer
                         line = Trim(sr.ReadLine())
                         ReDim MiArray(line.Length)
+
                         While (Not line Is Nothing And line <> "")
                             For i = 0 To line.Length - 1
-                                VDatos = line(i)
+                                VDatos = line(i).ToString
                                 MiArray(i) = VDatos
+
+                                ModeArray = calculateMode(line)
+                                mode.Items.Add(New ListItem("Line [" & count_line & "] mode= " & ModeArray(i) & " words", count_line))
                             Next
                             count_line += 1
-                            wordCount.Items.Add(New ListItem("Line [" & count_line & "] have " & line.Length & " words", count_line))
+                            wordCount.Items.Add(New ListItem("Line [" & count_line & "] have " & Trim(line.Length) & " words", count_line))
+
                             line = Trim(sr.ReadLine())
                         End While
 
@@ -72,10 +79,22 @@ Public Class Stadistics_about_text_files
         wordCount.Items.Clear()
     End Function
 
-    Public Function MeanMedianMode(ByRef location As String, ByVal countLine As Integer) As Boolean
-        Dim check As Boolean = False
-
-
-        Return check
+    Public Function calculateMode(ByRef line As String) As Integer() 'Hola me llamo Jose
+        Dim MyArray(line.Length / 2) As Integer '18
+        For i = 0 To line.Length - 1
+            Dim primeraPalabra As String = line(i)
+            For z = i + 1 To line.Length / 2
+                MyArray(z) = 1
+                If ((primeraPalabra = line(z) And primeraPalabra <> "")) Then
+                    MyArray(z) += 1
+                End If
+            Next
+        Next
+        Return MyArray 'solucion, crea array de palabras y comparas y sumas si esta..
     End Function
 End Class
+
+'0 H
+'1 o
+'2 l
+'3 a
